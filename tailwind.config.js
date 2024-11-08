8e87d055f619ce9e3d1ec94edbd70ff601de3878
Steve Halstead <github-contact@hashbangsystems.com>
2024-11-08 17:29:32 +0000

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**.{htm,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'keyboard': "url('./images/pexels-semac-14011035.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
